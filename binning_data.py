7ed884d9aaed84c3c90c274a079579eb04b805cd
Jeffrey <mym226@nyu.edu>
2019-06-20 12:26:54 -0700

    minor fixes

--- binning_data.py
+++ binning_data.py
@@ -2,9 +2,9 @@ import numpy as np
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 from plot_tracks import show_plot, plot_tracks
-
+import cartopy
 fakelons = 180 - np.random.random(50000)*360
-fakelats = (np.random.random(50000)*15+75)*-1
+fakelats = (np.random.random(50000)*30+60)*-1
 fake_var = (np.random.random(50000)*0.7+0.3)*fakelons
 
 
@@ -34,31 +34,37 @@ gs00 = gs0[0].subgridspec(1, 6)
 gs01 = gs0[1].subgridspec(1, 6)
 
 ax1 = fig.add_subplot(gs00[0,1:5], projection=ccrs.SouthPolarStereo())
+show_plot(ax1)
+
 # .subplot(2,2,(1,2), projection=ccrs.SouthPolarStereo())	
 plt.title("Raw data, with noise added")
 plot_tracks(fakelons, fakelats, variable=fake_var, cmap='bwr')
 cbar0 = plt.colorbar(ax=ax1)
 cbar0.set_label("Raw longitude")
-show_plot(ax1)
 
 # ax2 = plt.subplot(223, projection=ccrs.SouthPolarStereo())	
 ax2 = fig.add_subplot(gs01[0,:3], projection=ccrs.SouthPolarStereo())
-
+show_plot(ax2)
 plt.title("Binned (mean) data")
 data_crs = ccrs.PlateCarree()
 xx, yy = np.meshgrid(lon_range, lat_range)
 plt.pcolormesh(xx, yy, meaned_data.T, transform=data_crs, cmap='bwr')
 cbar = plt.colorbar(ax=ax2)
 cbar.set_label("Colorbar Label (unit)")
-show_plot(ax2)
 
 # ax3 = plt.subplot(224, projection=ccrs.SouthPolarStereo())	
 ax3 = fig.add_subplot(gs01[0,3:], projection=ccrs.SouthPolarStereo())
+
+show_plot(ax3)
+
 plt.pcolormesh(xx, yy, std_data.T, transform=data_crs)
 cbar2 = plt.colorbar()
 cbar2.set_label("Standard deviation")
-show_plot(ax3)
 
+
+ax1.add_feature(cartopy.feature.COASTLINE)
+ax2.add_feature(cartopy.feature.COASTLINE)
+ax3.add_feature(cartopy.feature.COASTLINE, edgecolor='w')
 
 plt.tight_layout()
 plt.show()
--- plot_tracks.py
+++ plot_tracks.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.path as mpath
 
 def show_plot(ax1):
-	ax1.set_extent([-180, 180, -90, -65], ccrs.PlateCarree())
+	ax1.set_extent([-180, 180, -90, -60], ccrs.PlateCarree())
 	ax1.add_feature(cartopy.feature.LAND)
 	ax1.add_feature(cartopy.feature.OCEAN)
 	theta = np.linspace(0, 2*np.pi, 100)
@@ -15,33 +15,41 @@ def show_plot(ax1):
 	verts = np.vstack([np.sin(theta), np.cos(theta)]).T
 	circle = mpath.Path(verts * radius + center)
 	ax1.set_boundary(circle, transform=ax1.transAxes)
-	plt.show()
+	# plt.show()
+
 	return
 
-def plot_tracks(lon, lat, variable, time_range = None, **kwargs):
-	global ax1
+def plot_tracks(lon, lat, variable=None, **kwargs):
+	# global ax1
 	if variable is not None:
 		plt.scatter(lon, lat, c=variable, transform = ccrs.PlateCarree(), **kwargs)
 	else:
 		plt.plot(lon, lat, transform = ccrs.PlateCarree(), **kwargs)
 	return 
 
-#################################################
-os.chdir("./Small_data/")
-ls = glob.glob("*.h5")
-f = h5py.File(ls[0], 'r') 
-#################################################
+def main():
+	#################################################
+	os.chdir("./Small_data/")
+	ls = glob.glob("*.h5")
+	f = h5py.File(ls[0], 'r') 
+	#################################################
 
-lat = f['gt1l/lat'][:]
-lon = f['gt1l/lon'][:]
-bsnow_conf = f['gt1l/bsnow_conf'][:]
-dt = f['gt1l/t_sec'][:]
-h_li = f['gt1l/h_li'][:]
+	laser_strs = ['gt1l', 'gt1r', 'gt2l', 'gt2r', 'gt3l', 'gt3r']
+	ax1 = plt.subplot(111, projection=ccrs.SouthPolarStereo())	
 
-ax1 = plt.subplot(111, projection=ccrs.SouthPolarStereo())	
-plot_tracks(lon, lat, bsnow_conf)
-plot_tracks(lon, lat, None, c='r')
-cbar = plt.colorbar()
-cbar.set_label("Colorbar Label")
-show_plot(ax1)
-# ax1.pcolormesh(xx, yy, variables[j], transform=data_crs)
+	for i in range(len(laser_strs)):
+		lastr = laser_strs[i]
+		lat = f[lastr+'/lat'][:]
+		lon = f[lastr+'/lon'][:]
+		bsnow_conf = f[lastr+'/bsnow_conf'][:]
+		dt = f[lastr+'/t_sec'][:]
+		h_li = f[lastr+'/h_li'][:]
+		plot_tracks(lon, lat, bsnow_conf)
+		plot_tracks(lon, lat, None)
+
+
+	cbar = plt.colorbar()
+	cbar.set_label("Colorbar Label")
+	show_plot(ax1)
+	plt.show()
+	# ax1.pcolormesh(xx, yy, variables[j], transform=data_crs)
